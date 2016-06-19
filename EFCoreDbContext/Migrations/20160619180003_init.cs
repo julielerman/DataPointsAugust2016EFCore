7f79b93bf5c19f39aeeb3d27e5a2e793ac5324fa
julie <julielerman@gmail.com>
2016-06-19 14:04:29 -0400

    Getting Migrations Working: modified project.jsons and added a fake static void main to get around limitation of preview toolingn so that I could use migrations in powershell. See notes in FakeProgram.cs.

---
+++ EFCoreDbContext/Migrations/20160619180003_init.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EFCoreDbContext.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Makers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Makers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Samurais",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Samurais", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Quotes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    SamuraiId = table.Column<int>(nullable: false),
+                    Text = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quotes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Quotes_Samurais_SamuraiId",
+                        column: x => x.SamuraiId,
+                        principalTable: "Samurais",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SamuraiBattle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BattleId = table.Column<int>(nullable: false),
+                    DateJoined = table.Column<DateTime>(nullable: false),
+                    SamuraiId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SamuraiBattle", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SamuraiBattle_Samurais_SamuraiId",
+                        column: x => x.SamuraiId,
+                        principalTable: "Samurais",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Secrets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    RealName = table.Column<string>(nullable: true),
+                    SamuraiId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Secrets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Secrets_Samurais_SamuraiId",
+                        column: x => x.SamuraiId,
+                        principalTable: "Samurais",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Swords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    MakerId = table.Column<int>(nullable: false),
+                    SamuraId = table.Column<int>(nullable: false),
+                    SamuraiId = table.Column<int>(nullable: true),
+                    WeightGrams = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Swords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Swords_Makers_MakerId",
+                        column: x => x.MakerId,
+                        principalTable: "Makers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Swords_Samurais_SamuraiId",
+                        column: x => x.SamuraiId,
+                        principalTable: "Samurais",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Quotes_SamuraiId",
+                table: "Quotes",
+                column: "SamuraiId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SamuraiBattle_SamuraiId",
+                table: "SamuraiBattle",
+                column: "SamuraiId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Secrets_SamuraiId",
+                table: "Secrets",
+                column: "SamuraiId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Swords_MakerId",
+                table: "Swords",
+                column: "MakerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Swords_SamuraiId",
+                table: "Swords",
+                column: "SamuraiId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quotes");
+
+            migrationBuilder.DropTable(
+                name: "SamuraiBattle");
+
+            migrationBuilder.DropTable(
+                name: "Secrets");
+
+            migrationBuilder.DropTable(
+                name: "Swords");
+
+            migrationBuilder.DropTable(
+                name: "Makers");
+
+            migrationBuilder.DropTable(
+                name: "Samurais");
+        }
+    }
+}
